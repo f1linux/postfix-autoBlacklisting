ed50746693a4b1e6fed47b25dfb7a9d55572214d
Terrence Houlahan <houlahan@F1Linux.com>
2020-06-27 22:19:14 +0100

    postfix-autoBlacklisting.sh: Material Changes: A) Consolidated 3 Tests for  abusers down to 2 B) Changed SystemD service type from "simple" to "oneshot" & correspondingly removed restart of it C) Improved descriptions of SystemD Service & Timer which fire the blacklisting script  D) Bumped the version number up in anticipation of the next release

--- postfix-autoBlacklisting.sh
+++ postfix-autoBlacklisting.sh
@@ -5,7 +5,7 @@
 
 # License: GPL 3.0
 # Source:  https://github.com/f1linux/postfix-autoBlacklisting.git
-# Version: 03.00.00
+# Version: 03.10.00
 
 cat <<'EOF'> /etc/postfix/access-autoBlacklisting.sh
 #!/bin/bash
@@ -23,6 +23,9 @@ cat <<'EOF'> /etc/postfix/access-autoBlacklisting.sh
 # This ensures restrictive access rules applied before permissive grants.
 # Consult README.md file for more granular detail about this script.
 
+# Uncomment for debugging purposes if tinkering with the script
+# If you tail /var/log/messages you can see how the script behaves after the SystemD Timer kicks it off:
+#set -x
 
 # Stop timer from executing Blacklisting script: Existing blacklist in /etc/postfix/access is enforce enforced while the new blacklist rebuilds
 systemctl stop Postfix-AutoBlacklisting.timer
@@ -31,19 +34,25 @@ systemctl stop Postfix-AutoBlacklisting.timer
 sed -i '/REJECT$/d' /etc/postfix/access
 
 ### Scrape log for different forms of abuse using different tests to identify abuse IPs and squirt each to same central file:
-# Enable/Disable any of provided block tests according to your requirements. Adding your own is easy if you use my tests which isolate offending IPs as templates.
+# Enable/Disable any of below tests according to your requirements. Adding your own is easy if you use my tests which isolate offending IPs as templates.
 
-# TEST 1: Blacklist Zombie hosts from endlessly squirting spam: These are identified by no PTR record being set for them
-#grep "does not resolve to address" /var/log/maillog | awk '{print $14}' | sed 's/://' | sort -u -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n | sed '/^$/d' >> /etc/postfix/access-blacklist
-grep "does not resolve to address" /var/log/maillog | awk '{print $14}' | sed 's/://' >> /etc/postfix/access-blacklist
+# TEST 1: Blacklist Zombie hosts from endlessly squirting spam: These are identified by no PTR record being set for them.
+# This test will catch both new zombies as well as those already RBLed which should serve to stop them constantly being endlessly checked against the RBL
+# IPv4 Test:
+# Below commented test was found to not be 100 perecent as accurate as the one using the awk form. Have not investigated why however.
+#grep "does not resolve to address" /var/log/maillog | grep -Eo "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort -u >> /etc/postfix/access-blacklist
+grep "does not resolve to address" /var/log/maillog | awk 'match($0, /([0-9]{1,3}[\.]){3}[0-9]{1,3}/) {print substr($0, RSTART, RLENGTH)}' | sort -u >> /etc/postfix/access-blacklist
+# IPv6 Test:
+grep "does not resolve to address" /var/log/maillog | grep -Eo "2[0-9a-fA-F]{3}:(([0-9a-fA-F]{1,4}[:]{1,2}){1,6}[0-9a-fA-F]{1,4})" | sort -u | awk '{print "["$1"]"}' >> /etc/postfix/access-blacklist
 
-# TEST 2: Block all subsequent attempts of RBL Blacklisted IPs from talking to our mailserver:
-grep "blocked using" /var/log/maillog | sed -rn 's/.*\[(([0-9]{,3}.){4})\].*/\1/gp' >> /etc/postfix/access-blacklist
 
-# TEST 3: Block spammers guessing account names where they know our domain:
+# TEST 2: Block spammers guessing account names where they know our domain:
 # WARNING: this could potentially cause a block where an unintentional misspelling of an mail account name occured.
-# Uncomment only if you are happy to accept such a risk:
+# Uncomment only if you are OK with accepting such a risk:
+# IPv4 Test:
 grep "Recipient address rejected: User unknown in virtual mailbox table" /var/log/maillog | sed -rn 's/.*\[(([0-9]{,3}.){4})\].*/\1/gp' >> /etc/postfix/access-blacklist
+# IPv6 Test:
+grep "Recipient address rejected: User unknown in virtual mailbox table" /var/log/maillog | grep -Eo "2[0-9a-fA-F]{3}:(([0-9a-fA-F]{1,4}[:]{1,2}){1,6}[0-9a-fA-F]{1,4})" | sort -u | awk '{print "["$1"]"}' >> /etc/postfix/access-blacklist
 
 
 # Populate an array with sorted and depuplicated list of offending IPs scraped from maillog using foregoing tests:
@@ -68,7 +77,6 @@ postmap /etc/postfix/access
 # After cycle completes and IPs written to /etc/postfix/acces we wipe array which repopulates anew upon next script execution:
 unset arrayIPblacklist
 
-systemctl restart Postfix-AutoBlacklisting.service
 systemctl start Postfix-AutoBlacklisting.timer
 
 EOF
@@ -79,13 +87,14 @@ chmod 744 /etc/postfix/access-autoBlacklisting.sh
 
 cat <<EOF> /etc/systemd/system/Postfix-AutoBlacklisting.service
 [Unit]
-Description=Captures IPs of hosts from maillog which match tests and prepends them to Postfix access file for Blacklisting.
+Description=Postfix-AutoBlacklisting Service scrapes maillog for IPs matching abuse tests and writes each to Postfix access file with REJECT appended after it
 
 [Service]
 User=root
 Group=root
-Type=simple
+Type=oneshot
 ExecStart=/bin/bash /etc/postfix/access-autoBlacklisting.sh
+#Environment=SYSTEMD_LOG_LEVEL=debug
 
 [Install]
 WantedBy=multi-user.target
@@ -98,7 +107,7 @@ chmod 644 /etc/systemd/system/Postfix-AutoBlacklisting.service
 
 cat <<EOF> /etc/systemd/system/Postfix-AutoBlacklisting.timer
 [Unit]
-Description=Executes /etc/postfix/access-autoBlacklisting.sh
+Description=Postfix-AutoBlacklisting.timer executes /etc/postfix/access-autoBlacklisting.sh
 
 [Timer]
 # Execute script every 90 seconds to limits number of times a spammer can connect before being blocked after this interval is reached:
